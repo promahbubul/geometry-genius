46890d81c3a5446da0f7a33e1df7c9760b1ac46c
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-08-18 21:42:06 +0600

    container add item section compleate

--- script/area-calculator.js
+++ script/area-calculator.js
@@ -3,51 +3,93 @@
  * 2. get base element and convert it number
  * 3. get height element and convert it number
  * 4. calculate: 0.5 * base * height
- * 
+ *
  */
 
-
 function getInputElement(id) {
-    const inputElement = document.getElementById(id);
-    const inputValueString = inputElement.value;
-    const inputValue = parseFloat(inputValueString);
-    return inputValue;
+  const inputElement = document.getElementById(id);
+  const inputValueString = inputElement.value;
+  const inputValue = parseFloat(inputValueString);
+  return inputValue;
 }
 
 function setElement(id, area) {
-    const element = document.getElementById(id);
-    return element.innerText = area;
+  const element = document.getElementById(id);
+  return (element.innerText = area);
 }
 
-
 // Triangle
-document.getElementById('triangle-calculate').addEventListener('click', function() {
-    const base = getInputElement('traingle-base')
-    const height = getInputElement('traingle-height')
-    const triangleCal = 0.5 * base * height
-   setElement('triangle-area', triangleCal)
-})
+document
+  .getElementById("triangle-calculate")
+  .addEventListener("click", function () {
+    const base = getInputElement("traingle-base");
+    const height = getInputElement("traingle-height");
+    if (isNaN(base) || isNaN(height)) {
+      return alert("Please input only number.");
+    }
+    const triangleCal = 0.5 * base * height;
+    setElement("triangle-area", triangleCal);
+    itemAdded("triangle", triangleCal);
+  });
 
 // Reactangle
-document.getElementById('rectangle-calculate').addEventListener('click', function() {
-    const width = getInputElement('rectangle-width')
-    const length = getInputElement('rectangle-length')
+document
+  .getElementById("rectangle-calculate")
+  .addEventListener("click", function () {
+    const width = getInputElement("rectangle-width");
+    const length = getInputElement("rectangle-length");
+    if (isNaN(width) || isNaN(length)) {
+      return alert("Please input only number.");
+    }
     const calculation = width * length;
-    setElement('rectangle-area', calculation)
-})
+    setElement("rectangle-area", calculation);
+    itemAdded("rectangle", calculation);
+    
+  });
 
 // Parallelogram
-document.getElementById('parallelogram-calculate').addEventListener('click', function() {
-    const base = getInputElement('parallelogram-base')
-    const height = getInputElement('parallelogram-height')
+document
+  .getElementById("parallelogram-calculate")
+  .addEventListener("click", function () {
+    const base = getInputElement("parallelogram-base");
+    const height = getInputElement("parallelogram-height");
+    if (isNaN(base) || isNaN(height)) {
+      return alert("Please input only number.");
+    }
     const calculation = base * height;
-    setElement('parallelogram-area', calculation)
-})
+    setElement("parallelogram-area", calculation);
+    itemAdded("parallelogram", calculation);
+  });
 
-// ellipse 
-document.getElementById('ellipse-calculate').addEventListener('click', function() {
-    const roundFrist = getInputElement('ellipse-frist-area')
-    const roundSecond = getInputElement('ellipse-second-area')
-    const calculation =  (Math.PI*roundFrist*roundSecond).toFixed(2)
-    setElement('ellipse-area', calculation)
-})
+// ellipse
+document
+  .getElementById("ellipse-calculate")
+  .addEventListener("click", function () {
+    const roundFrist = getInputElement("ellipse-frist-area");
+    const roundSecond = getInputElement("ellipse-second-area");
+    if (isNaN(roundFrist) || isNaN(roundSecond)) {
+      return alert("Please input only number.");
+    }
+    const calculation = (Math.PI * roundFrist * roundSecond).toFixed(2);
+    setElement("ellipse-area", calculation);
+    itemAdded("ellipse", calculation);
+  });
+
+// add to calculation entry
+function itemAdded(areaName, area) {
+  const itemContainer = document.getElementById("calculate-container");
+  const count = itemContainer.childElementCount;
+  const itemName = document.getElementById(areaName).innerText;
+  const p = document.createElement("p");
+  p.classList.add("mt-2");
+  itemContainer.appendChild(p).innerHTML = `
+   <div class="flex text-sm flex-row items-center justify-between">
+   <div>${count + 1}. ${itemName}  </div>
+   <div>
+   ${area} 
+   <span>cm<sup>2</sup></span>
+   </div>
+   <div><button id="convert-mm" class="btn  bg-blue-500 btn-sm text-xs text-white normal-case"><span>Convert to m<sup>2</sup></span></button>
+   </div>
+   </div>`;
+}
